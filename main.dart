01c1030c3ccefd63ae8553a6ec0b02bdb3798152
Ali-Shoaib-MD <ali.shoaib@nomw-health.co.uk>
2024-08-02 07:17:56 +0500

    Made some initial comments on the main.dart file to test the git commit.

--- main.dart
+++ main.dart
@@ -1,11 +1,17 @@
 void main() {
+  /*
+  This code gives examples of the built-in methods of Dart Lists
+*/
+
+//Defining arbitray lists
   List animals = ['duck', 'lion', 'elephant', 'monkey', 345, 644, 759];
   List mamals = ['Baboon', 'Whale', 'Dolphin', 'Gorilla', 555, 567, 0.78];
 
+//.isNotEmpty returns a boolean value based on whether the list is empty or not
   if (animals.isNotEmpty) {
     print("The list is not empty");
   }
-
+//.length returns the size of the list
   print(animals.length);
 
   print(animals.last);
